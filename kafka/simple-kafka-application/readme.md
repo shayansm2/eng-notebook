273132011ffba1ed1d063c657aef86490a40b658
shayansm <s.shafieemoghadam@digikala.com>
2022-09-23 16:54:28 +0330

    send messages with callback

---
+++ kafka/simple-kafka-application/readme.md
@@ -0,0 +1,4 @@
+This is a java playground for playing with kafka
+
+For actual codes of the course I wrote this from, visit this
+[link](https://github.com/conduktor/kafka-beginners-course/tree/main/kafka-basics/src/main/java/io/conduktor/demos/kafka).
--- kafka/simple-kafka-application/src/main/java/org/example/Main.java
+++ kafka/simple-kafka-application/src/main/java/org/example/Main.java
@@ -2,16 +2,39 @@ package org.example;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main {
     public static void main(String[] args) {
         sendMessageToProducer("new_with_keys", "is_this_working?");
+        sendMessageToProducerWithCallback("new_with_keys", "is_this_working_again?");
     }
 
+    //    actual code of this part
+    //    https://github.com/conduktor/kafka-beginners-course/blob/main/kafka-basics/src/main/java/io/conduktor/demos/kafka/ProducerDemo.java
     public static void sendMessageToProducer(String topic, String message) {
         KafkaProducer<String, String> producer = Producer.getInstance();
         ProducerRecord<String, String> record = new ProducerRecord<>(topic, message);
         producer.send(record);
+        producer.flush();
+        producer.close();
+    }
+
+    //    actual code of this part
+    //    https://github.com/conduktor/kafka-beginners-course/blob/main/kafka-basics/src/main/java/io/conduktor/demos/kafka/ProducerDemoWithCallback.java
+    public static void sendMessageToProducerWithCallback(String topic, String message) {
+        Logger logger = LoggerFactory.getLogger(Producer.class.getSimpleName());
+
+        KafkaProducer<String, String> producer = Producer.getInstance();
+        ProducerRecord<String, String> record = new ProducerRecord<>(topic, message);
+        producer.send(record, (recordMetadata, e) -> {
+            if (e == null) {
+                logger.info("topic:partition " + recordMetadata.topic() + ":" + recordMetadata.partition());
+            } else {
+                logger.error("exception", e);
+            }
+        });
         producer.flush();
         producer.close();
     }
